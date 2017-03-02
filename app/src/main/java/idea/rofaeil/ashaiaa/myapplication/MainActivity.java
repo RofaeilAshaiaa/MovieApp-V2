38dbe0caa009080c24ef89d0cc7fbb9721ec2a8d
Rofaei Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-03 01:40:50 +0200

    Initial commit

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
@@ -1,13 +1,21 @@
 package idea.rofaeil.ashaiaa.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/TopRatedFragment.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/TopRatedFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainActivityFragment extends Fragment {
+public class TopRatedFragment extends Fragment {
 
 
-    public MainActivityFragment() {
+    public TopRatedFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_activity_fragment, container, false);
+        return inflater.inflate(R.layout.top_rated_fragment, container, false);
     }
 
 }
