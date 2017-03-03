b1aa25ff34759e72374e460c655dd63e456d4572
Rofaei Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-03 02:14:38 +0200

    Initial commit

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
@@ -1,21 +1,45 @@
 package idea.rofaeil.ashaiaa.myapplication;
 
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import idea.rofaeil.ashaiaa.myapplication.databinding.MainActivityBinding;
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView mBottomNavigation;
+    private Fragment mFragment;
+    private FragmentManager mFragmentManager;
+    private MainActivityBinding mBinding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.main_activity);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        int id = item.getItemId();
+        switch (id){
+            case R.id.favorites:
+                //fragment = new SearchFragment();
+                break;
+            case R.id.top_rated:
+                mFragment = new TopRatedFragment();
+                break;
+            case R.id.popular:
+                //fragment = new DealsFragment();
+                break;
+        }
+//        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.main_container, fragment).commit();
+        return true;
     }
 }
