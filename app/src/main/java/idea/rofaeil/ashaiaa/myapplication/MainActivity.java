f98f55778e393ae60b2c753ed2148c28fd155456
Rofaei Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-03 02:44:31 +0200

    Initial commit

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
@@ -11,40 +11,42 @@ import android.view.MenuItem;
 
 import idea.rofaeil.ashaiaa.myapplication.databinding.MainActivityBinding;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView mBottomNavigation;
     private Fragment mFragment;
-    private FragmentManager mFragmentManager;
     private MainActivityBinding mBinding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+        setClickListenerNavigationBottom();
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case R.id.favorites:
-                mFragment = new FavouriteFragment();
-                break;
-            case R.id.top_rated:
-                mFragment = new TopRatedFragment();
-                break;
-            case R.id.popular:
-                mFragment = new PopularFragment();
-                break;
-        }
+    private void setClickListenerNavigationBottom() {
+        mBinding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.favorites:
+                        mFragment = new FavouriteFragment();
+                        break;
+                    case R.id.top_rated:
+                        mFragment = new TopRatedFragment();
+                        break;
+                    case R.id.popular:
+                        mFragment = new PopularFragment();
+                        break;
+                }
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(mBinding.mainContainer.getId(), mFragment)
-                .commit();
-//        final FragmentTransaction transaction = fragmentManager.beginTransaction();
-//        transaction.replace(R.id.main_container, fragment).commit();
-        return true;
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_container, mFragment)
+                        .commit();
+                return true;
+            }
+        });
     }
+
 }
