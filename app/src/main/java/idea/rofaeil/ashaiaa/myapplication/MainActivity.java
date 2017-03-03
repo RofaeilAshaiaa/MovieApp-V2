49c58cabc0b9c04ae8f4030a7d18d1f81dbc6b9e
Rofaei Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-03 03:34:48 +0200

    Initial commit

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
