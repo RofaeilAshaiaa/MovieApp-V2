2ff5f0844405a7594009951ddd4dedc4c96b1fbb
Rofaei Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-05 21:36:12 +0200

    add parceler annotation to movie class

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/HelperAndAdapters/Movie.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/HelperAndAdapters/Movie.java
@@ -4,20 +4,42 @@ package idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 
+@Parcel
 public class Movie {
 
-    private int mMovieId;
-    private String mMoviePoster;
-    private String mMovieOverview;
-    private String mReleaseDate;
-    private String mMovieRuntime;
-    private String mOriginalTitle;
-    private String mVoteAverage;
+    public int mMovieId;
+    public String mMoviePoster;
+    public String mMovieOverview;
+    public String mReleaseDate;
+    public String mMovieRuntime;
+    public String mOriginalTitle;
+    public String mVoteAverage;
 
     public Movie() {
+    }
+
+    public static ArrayList<Movie> extractMovieDataFromJson(JSONArray movie_jsonArray) throws JSONException {
+        int ctr = movie_jsonArray.length();
+        ArrayList<Movie> moviesListInMethod = new ArrayList<>(ctr);
+        //ArrayList<Movie> movies_ArrayList_in_method = new ArrayList<>();
+
+        for (int i = 0; i < ctr; i++) {
+            Movie mMovie = new Movie();
+
+            JSONObject jsonObject_of_movie = movie_jsonArray.getJSONObject(i);
+
+            StringBuilder URI = new StringBuilder("http://image.tmdb.org/t/p/");
+            URI.append("w185/").append(jsonObject_of_movie.getString("poster_path"));
+
+            mMovie.setMoviePoster(URI.toString());
+            mMovie.setMovieId(jsonObject_of_movie.getInt("id"));
+            moviesListInMethod.add(mMovie);
+        }
+        return moviesListInMethod;
     }
 
     public String getMoviePoster() {
@@ -74,21 +96,5 @@ public class Movie {
 
     public void setVoteAverage(String mVoteAverage) {
         this.mVoteAverage = mVoteAverage;
-    }
-
-    private ArrayList<Movie> extractMovieDataFromJson(JSONArray movie_jsonArray) throws JSONException {
-        int ctr = movie_jsonArray.length();
-        ArrayList<Movie> moviesListInMethod = new ArrayList<>(ctr);
-        //ArrayList<Movie> movies_ArrayList_in_method = new ArrayList<>();
-
-        for (int i = 0; i < ctr; i++) {
-            Movie mMovie = new Movie();
-
-            JSONObject jsonObject_of_movie = movie_jsonArray.getJSONObject(i);
-            mMovie.setMoviePoster(jsonObject_of_movie.getString("poster_path"));
-            mMovie.setMovieId(jsonObject_of_movie.getInt("id"));
-            moviesListInMethod.add(mMovie);
-        }
-        return moviesListInMethod;
     }
 }
