5ab394cf60178b5110ee97668b789eb153d8648e
Rofaei Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-06 01:57:45 +0200

    1-set-up loader and its methods to load data and view Movies images 2-show progress-bar according to loader process

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/PopularFragment.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/PopularFragment.java
@@ -1,50 +1,87 @@
 package idea.rofaeil.ashaiaa.myapplication.MainClasses;
 
-
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters.Movie;
+import idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters.NetworkAsyncTaskLoader;
 import idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters.RecyclerViewAdapter;
 import idea.rofaeil.ashaiaa.myapplication.R;
 import idea.rofaeil.ashaiaa.myapplication.databinding.PopularFragmentBinding;
 
+public class PopularFragment extends Fragment implements LoaderManager.LoaderCallbacks<String>{
 
-public class PopularFragment extends Fragment {
+    private final int POPULAR_LOADER_ID = 11;
+    private PopularFragmentBinding mBinding ;
+    private ProgressBar mProgressBar ;
 
-
-    private  PopularFragmentBinding mBinding ;
     public PopularFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mBinding = DataBindingUtil.inflate(inflater,R.layout.popular_fragment ,container,false) ;
-
+        mProgressBar = (ProgressBar) getActivity().findViewById(R.id.progress_bar_review_subjects);
+        makeNetworkRequest();
         return mBinding.getRoot() ;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mBinding.rvPopular.setAdapter( new RecyclerViewAdapter(new ArrayList<Movie>(4) ,getContext() ,getActivity()));
+    private void makeNetworkRequest() {
 
-        GridLayoutManager mLayoutManager = new GridLayoutManager(getContext(),2);
+        mProgressBar.setVisibility(View.VISIBLE);
+        LoaderManager loaderManager = getActivity().getSupportLoaderManager() ;
+        Loader<String> popularLoader = loaderManager.getLoader(POPULAR_LOADER_ID);
+        loaderManager.initLoader(POPULAR_LOADER_ID,null,this).forceLoad();
+        
+//        if(popularLoader == null)
+//        {
+//          loaderManager.initLoader(POPULAR_LOADER_ID,null,this).forceLoad();
+//        }else {
+//            loaderManager.restartLoader(POPULAR_LOADER_ID,null,this).startLoading();
+//        }
 
-        mBinding.rvPopular.setLayoutManager(mLayoutManager);
     }
 
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new NetworkAsyncTaskLoader(getContext() ,MainActivity.URLs[0]);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+
+        JSONObject mJsonObject;
+        try {
+            mJsonObject = new JSONObject(data);
+            JSONArray mMoviesJsonArray = mJsonObject.getJSONArray("results");
+            ArrayList<Movie> mMoviesList = Movie.extractMovieDataFromJson(mMoviesJsonArray);
+            mBinding.rvPopular.setAdapter( new RecyclerViewAdapter(mMoviesList,getContext() ));
+            GridLayoutManager mLayoutManager = new GridLayoutManager(getContext(),2);
+            mBinding.rvPopular.setLayoutManager(mLayoutManager);
+        } catch (JSONException e) {
+        }
+
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
 }
--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/TopRatedFragment.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/TopRatedFragment.java
@@ -1,26 +1,33 @@
 package idea.rofaeil.ashaiaa.myapplication.MainClasses;
 
-
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters.Movie;
+import idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters.NetworkAsyncTaskLoader;
 import idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters.RecyclerViewAdapter;
 import idea.rofaeil.ashaiaa.myapplication.R;
 import idea.rofaeil.ashaiaa.myapplication.databinding.TopRatedFragmentBinding;
 
+public class TopRatedFragment extends Fragment implements LoaderManager.LoaderCallbacks<String> {
 
-public class TopRatedFragment extends Fragment {
-
+    private final int TOP_RATED_LOADER_ID = 22;
     private TopRatedFragmentBinding mBinding;
-
+    private ProgressBar mProgressBar ;
 
     public TopRatedFragment() {
         // Required empty public constructor
@@ -28,22 +35,54 @@ public class TopRatedFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         mBinding = DataBindingUtil.inflate(inflater, R.layout.top_rated_fragment, container, false);
-
+        mProgressBar = (ProgressBar) getActivity().findViewById(R.id.progress_bar_review_subjects);
+        makeNetworkRequest();
         return mBinding.getRoot();
 
     }
+
     @Override
-    public void onStart() {
-        super.onStart();
-        mBinding.rvTopRated.setAdapter( new RecyclerViewAdapter(new ArrayList<Movie>(4) ,getContext() ,getActivity()));
-
-        GridLayoutManager mLayoutManager = new GridLayoutManager(getContext(),2);
-
-        mBinding.rvTopRated.setLayoutManager(mLayoutManager);
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new NetworkAsyncTaskLoader(getContext() ,MainActivity.URLs[1]);
     }
 
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        JSONObject mJsonObject;
+        try {
+            mJsonObject = new JSONObject(data);
+            JSONArray mMoviesJsonArray = mJsonObject.getJSONArray("results");
+            ArrayList<Movie> mMoviesList = Movie.extractMovieDataFromJson(mMoviesJsonArray);
+            mBinding.rvTopRated.setAdapter( new RecyclerViewAdapter(mMoviesList,getContext() ));
+            GridLayoutManager mLayoutManager = new GridLayoutManager(getContext(),2);
+            mBinding.rvTopRated.setLayoutManager(mLayoutManager);
+        } catch (JSONException e) {
+        }
+
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+    private void makeNetworkRequest() {
+
+        mProgressBar.setVisibility(View.VISIBLE);
+        LoaderManager loaderManager = getActivity().getSupportLoaderManager() ;
+        Loader<String> popularLoader = loaderManager.getLoader(TOP_RATED_LOADER_ID);
+        loaderManager.initLoader(TOP_RATED_LOADER_ID,null,this).forceLoad();
+
+//        if(popularLoader == null)
+//        {
+//          loaderManager.initLoader(POPULAR_LOADER_ID,null,this).forceLoad();
+//        }else {
+//            loaderManager.restartLoader(POPULAR_LOADER_ID,null,this).startLoading();
+//        }
+
+    }
 }
