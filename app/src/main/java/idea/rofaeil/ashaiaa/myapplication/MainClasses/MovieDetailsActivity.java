a434a8622d150062616506e2a6f517dc40be324b
Rofaeil Ashaiaa <rofaeilashaiaa@gmail.com>
2017-03-12 01:29:39 -0800

    initial commit

--- app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/MovieDetailsActivity.java
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/MovieDetailsActivity.java
@@ -1,15 +1,41 @@
 package idea.rofaeil.ashaiaa.myapplication.MainClasses;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import idea.rofaeil.ashaiaa.myapplication.R;
+import idea.rofaeil.ashaiaa.myapplication.databinding.MovieDetailsActivityBinding;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
+    private MovieDetailsActivityBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.movie_details_activity);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.movie_details_activity);
+        setSupportActionBar(mBinding.tbMovieDetailsActivity);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra(getString(R.string.movie_id_string));
+
+        Bundle bundle = new Bundle() ;
+        bundle.putString(getString(R.string.movie_id_string),id);
+
+        Fragment mFragment = new MovieDetailsFragment();
+        mFragment.setArguments(bundle);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.details_fragment_container, mFragment)
+                .commit();
     }
 }
