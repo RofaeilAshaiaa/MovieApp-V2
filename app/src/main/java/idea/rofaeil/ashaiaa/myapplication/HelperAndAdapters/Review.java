aa63b9a9f9009ef91656e77afb60987de87d459f
Rofaeil Ashaiaa <rofaeilashaiaa@gmail.com>
2017-03-13 12:50:53 -0700

    added Review Object Class

---
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/HelperAndAdapters/Review.java
@@ -0,0 +1,35 @@
+package idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters;
+
+/**
+ * Created by Matrix on 3/12/2017.
+ */
+
+public class Review {
+
+    private String Author ;
+    private String ReviewContent ;
+
+    public Review() {
+    }
+
+    public Review(String author, String reviewContent) {
+        Author = author;
+        ReviewContent = reviewContent;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getReviewContent() {
+        return ReviewContent;
+    }
+
+    public void setReviewContent(String reviewContent) {
+        ReviewContent = reviewContent;
+    }
+}
