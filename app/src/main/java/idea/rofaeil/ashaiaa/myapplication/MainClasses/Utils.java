bdda03e46cd19cfaa4258b784e4a70aefedddc9b
Rofaeil Ashaiaa <rofaeilashaiaa@gmail.com>
2017-03-17 01:36:36 +0200

    create utils class and create isNetworkAvailable method to check Internet Connection

---
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/MainClasses/Utils.java
@@ -0,0 +1,23 @@
+package idea.rofaeil.ashaiaa.myapplication.MainClasses;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Matrix on 3/15/2017.
+ */
+
+public class Utils {
+
+    public static final String TAG = "mi5a";
+
+    //check if network available and connected to one
+    public static boolean isNetworkAvailable(FragmentActivity activity) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
