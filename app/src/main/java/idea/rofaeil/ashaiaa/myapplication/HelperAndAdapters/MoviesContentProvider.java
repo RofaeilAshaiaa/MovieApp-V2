4a20063e4b4ee93998be12e59a96c48184bb1889
Rofaeil Ashaiaa <rofaeilashaiaa@gmail.com>
2017-03-17 02:49:55 +0200

    added MoviesContentProvider class and declare it in the manifest file

---
+++ app/src/main/java/idea/rofaeil/ashaiaa/myapplication/HelperAndAdapters/MoviesContentProvider.java
@@ -0,0 +1,55 @@
+package idea.rofaeil.ashaiaa.myapplication.HelperAndAdapters;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Matrix on 3/17/2017.
+ */
+
+public class MoviesContentProvider extends ContentProvider {
+
+    private MoviesDbHelper mMoviesDbHelper ;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mMoviesDbHelper = MoviesDbHelper.getInstance(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection,
+                        @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection,
+                      @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
